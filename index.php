9be7ceaa5a04833d41aec1a5a689c677608e8886
cathautzen <cathautzen@gmail.com>
2023-03-29 13:43:10 +0200

    Carousel

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <script src="https://kit.fontawesome.com/645a3a1c42.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Oswald&family=Quicksand&family=Raleway:wght@200&display=swap" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
@@ -23,10 +26,10 @@
     <img src="images/v-logo.png" width="180">
 </div>
 
-<div class="container">
-    <div class="card border-0">
+<div class="container d-flex justify-content-center">
+    <div class="col-10 card border-0">
         <div class="card-body">
-            <h5 class="card-title">Velkommen til Vilma Filmfestival</h5>
+            <h5 class="card-title h2">Velkommen til Vilma Filmfestival</h5>
             <p class="card-text">Din go-to-app til dette års store film
                 oplevelse. Der bliver vist danske film,
                 interviews med skuespillere og spændende
@@ -43,8 +46,11 @@
 <br>
 
 
-<!-- koden til carousel -->
-<div class="container-fluid">
+<!-- koden til Film carousel -->
+<div class="text-center">
+    <h2>Årets Film</h2>
+</div>
+<div class="container col-10">
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -55,22 +61,22 @@
         <div class="carousel-item active">
             <img src="images/film1.jpg" class="d-block w-100" alt="...">
             <div class="carousel-caption">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+                <h5>Vil du se min smukke navle?</h5>
+                <p>Vises d. 2 April 2024</p>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="images/film1.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
+            <img src="images/film2.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption">
+                <h5>Adams Æbler</h5>
+                <p>Vises d. 3 April 2024</p>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="images/film1.jpg" class="d-block w-100" alt="...">
+            <img src="images/film3.jpg" class="d-block w-100" alt="...">
             <div class="carousel-caption ">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
+                <h5>Kundskabens træ</h5>
+                <p>Vises d. 5 April 2024</p>
             </div>
         </div>
     </div>
@@ -84,6 +90,75 @@
     </button>
 </div>
 </div>
+<br>
+
+<!-- koden til Gæster carousel -->
+<div class="text-center">
+    <h2 class="">Årets Gæster</h2>
+</div>
+<div class="container col-10">
+    <div id="carouselExampleCaptions2" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/person1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption">
+                    <h5>Sofie lassen kahlke</h5>
+                    <p>Taler d. 2 April 2024</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/person2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption">
+                    <h5>Søren Kragh-Jacobsen</h5>
+                    <p>Vises d. 3 April 2024</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/person3.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption ">
+                    <h5>Bille August</h5>
+                    <p>Vises d. 5 April 2024</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<br>
+<div class="text-center">
+    <img src="images/swirl-left.png" height="120">
+</div>
+<br>
+<div class="text-center">
+    <h2 class="">Events</h2>
+</div>
+
+<div class="container d-flex justify-content-center">
+    <div class="card border-0 col-10">
+        <div class="card-body ">
+            <h5 class="card-title">Outdoor Film Show
+                på Schweizerpladsen i Slagelse</h5>
+            <p class="card-text">
+                Vil du opleve et film show ud over det
+                sædvanlige, så holder vi i foråret 2024 vores
+                Outdoor Film Show i Slagelse by...</p>
+            <a class="btn btn-knap text-white fw-bold" href="#" role="button">Læs mere</a>
+        </div>
+    </div>
+</div>
+
 
 <?php include 'footer.php';?>
 
